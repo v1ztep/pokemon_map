749e3813dca83d43d0dde2f562877d2cb525cb35
v1ztep <v1ztep@gmail.com>
2020-04-12 16:38:00 +0300

    Создал покемонов и вернул для админки обязательность заполнения всех полей

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return self.title

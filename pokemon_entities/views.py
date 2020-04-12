8cd46d5672bf63fb977bbe24ad03592fd5e65c6d
v1ztep <v1ztep@gmail.com>
2020-04-12 22:15:16 +0300

    Вытащил покемонов из админки в шапке

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from .models import Pokemon
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -33,12 +34,19 @@ def show_all_pokemons(request):
                 pokemon['title_ru'], pokemon['img_url'])
 
     pokemons_on_page = []
-    for pokemon in pokemons:
-        pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
-        })
+    for pokemon in Pokemon.objects.all():
+        if pokemon.image:
+            pokemons_on_page.append({
+                'pokemon_id': pokemon.id,
+                'title_ru': pokemon.title,
+                'img_url': pokemon.image.url
+            })
+        else:
+            pokemons_on_page.append({
+                'pokemon_id': pokemon.id,
+                'title_ru': pokemon.title,
+            })
+
 
     return render(request, "mainpage.html", context={
         'map': folium_map._repr_html_(),

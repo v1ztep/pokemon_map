225762f07fc2b1b2f4ec3aa3b00f5ba060e6c602
v1ztep <v1ztep@gmail.com>
2020-04-12 19:18:00 +0300

    Добавил покемонам стату

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,7 +11,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
     Lat = models.FloatField()
     Lon = models.FloatField()
+
     Appeared_at = models.DateTimeField(default=None)
     Disappeared_at = models.DateTimeField(default=None)
+
+    Level = models.IntegerField(default=None)
+    Health = models.IntegerField(default=None)
+    Strength = models.IntegerField(default=None)
+    Defence = models.IntegerField(default=None)
+    Stamina = models.IntegerField(default=None)
